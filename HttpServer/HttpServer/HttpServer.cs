8c02701b3e62587ffea1625a06ef8952fa60d4a3
Zancrow20 <ildar1qweas@gmail.com>
2022-10-01 13:14:07 +0300

    httpServer for Google

---
+++ HttpServer/HttpServer/HttpServer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace HttpServer;
+
+public class HttpServer
+{
+   private readonly string _url;
+   private readonly HttpListener _listener;
+   private HttpListenerContext _httpListenerContext;
+   private bool isWorking;
+
+   public HttpServer(string url)
+   {
+      _listener = new HttpListener();
+      _url = url;
+      
+      _listener.Prefixes.Add(_url);
+   }
+
+   public void Start()
+   {
+      if (isWorking)
+      {
+         Console.WriteLine("Сервер уже работает");
+         return;
+      }
+
+      Console.WriteLine("Запуск сервера...");
+      _listener.Start();
+      isWorking = true;
+      Console.WriteLine("Сервер запущен");
+
+      Listen();
+   }
+   
+   public void Stop()
+   {
+      if(!isWorking) return;
+      isWorking = false;
+      Console.WriteLine("Остановка сервера...");
+      _listener.Stop();
+      Console.WriteLine("Сервер остановлен");
+   }
+
+   private void Listen()
+   {
+      _listener.BeginGetContext(new AsyncCallback(ListenerCallback), _listener);
+   }
+   
+      private void ListenerCallback(IAsyncResult result)
+   {
+      if (_listener.IsListening)
+      {
+         HttpListenerContext context = _listener.EndGetContext(result);
+         HttpListenerRequest request = context.Request;
+         // получаем объект ответа
+         HttpListenerResponse response = context.Response;
+         // создаем ответ в виде кода html
+         
+         response.Headers.Set("Content-Type", "text/html");
+         byte[] buffer = File.ReadAllBytes(@"./Google/google.html");
+         
+         // получаем поток ответа и пишем в него ответ
+         response.ContentLength64 = buffer.Length;
+         
+         Stream output = response.OutputStream;
+         output.Write(buffer, 0, buffer.Length);
+         
+         // закрываем поток
+         output.Close();
+         
+         Listen();
+      }
+   }
+}
---
+++ HttpServer/HttpServer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Buffers;
+using System.Net;
+using System.IO;
+using HttpServer;
+
+namespace NetConsoleApp
+{
+    class Program
+    {
+        private static bool _keepRunning = true;
+        static void Main(string[] args)
+        {
+            var url = "http://localhost:2323/google/";
+            HttpServer.HttpServer server = new HttpServer.HttpServer(url);
+            server.Start();
+            while (_keepRunning)
+            {
+                var request = Console.ReadLine()?.ToLower();
+                if (request == "start")
+                    server.Start();
+                if (request == "stop")
+                    server.Stop();
+                if (request == "exit")
+                    _keepRunning = false;
+            }
+            Console.WriteLine("Exiting gracefully...");
+            Console.Read();
+        }
+    }
+}
