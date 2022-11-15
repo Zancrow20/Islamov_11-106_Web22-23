524fc4db879f7e3f2038d66066991c865d21ff78
Zancrow20 <ildar1qweas@gmail.com>
2022-11-16 00:44:29 +0300

    commit-5

---
+++ week_9/HttpServer/Models/Session.cs
@@ -0,0 +1,17 @@
+﻿namespace HttpServer.Models;
+
+public class Session
+{
+    public int AccountId { get; }
+    public int? Id { get; }
+    public string Nickname { get; }
+    public DateTime CreateDateTime { get; }
+
+    public Session(int? id, int accountId, string nickname, DateTime createDateTime)
+    {
+        Id = id;
+        AccountId = accountId;
+        Nickname = nickname;
+        CreateDateTime = createDateTime;
+    }
+}
---
+++ week_9/HttpServer/Services/SessionManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Concurrent;
+using HttpServer.Models;
+using Microsoft.Extensions.Caching.Memory;
+namespace HttpServer;
+
+public class SessionManager
+{
+    private MemoryCache _cache = new MemoryCache(new MemoryCacheOptions());
+    private ConcurrentDictionary<object, SemaphoreSlim> _locks = new ();
+ 
+    public Session GetOrAdd(object key, Func<Session> createItem)
+    {
+        if (!_cache.TryGetValue(key, out Session cacheEntry))
+        {
+            SemaphoreSlim slim = _locks.GetOrAdd(key, k => new SemaphoreSlim(1, 1));
+            
+            slim.WaitAsync();
+            try
+            {
+                if (!_cache.TryGetValue(key, out cacheEntry))
+                {
+                    cacheEntry = createItem();
+                    var cacheEntryOptions =
+                        new MemoryCacheEntryOptions()
+                            .SetSlidingExpiration(TimeSpan.FromMinutes(2))
+                            .SetAbsoluteExpiration(TimeSpan.FromMinutes(2));
+                    _cache.Set(key, cacheEntry, cacheEntryOptions);
+                }
+            }
+            finally
+            {
+                slim.Release();
+            }
+        }
+        return cacheEntry;
+    }
+
+    public bool CheckSession(object key)
+    {
+        return _cache.TryGetValue(key, out _);
+    }
+
+    public Session? GetInfo(object key)
+    {
+        return _cache.TryGetValue(key, out Session session) ? session 
+            : throw new KeyNotFoundException("Couldn't find this key");
+    }
+}
